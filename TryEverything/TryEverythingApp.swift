432d3f97763ef0b2fdf7697cedad130b8b985d5f
Bhavin Patel <bbaspatel@icloud.com>
2023-01-05 10:19:00 -0600

    Initial Commit

---
+++ TryEverything/TryEverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryEverythingApp.swift
+//  TryEverything
+//
+//  Created by Bhavin Patel on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TryEverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
